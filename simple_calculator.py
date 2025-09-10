721e97ae35eb09bb7d6ccfd79455ce36cf896f83
xmohamad18z-gif <xmohamad18z@gmail.com>
2025-09-10 07:36:31 +0300

    simple calculator

---
+++ simple_calculator.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+if operation == "+":
+    print("Result:", num1 + num2)
+elif operation == "-":
+    print("Result:", num1 - num2)
+elif operation == "*":
+    print("Result:", num1 * num2)
+elif operation == "/":
+    print("Result:", num1 / num2)
+else:
+    print("Invalid operation!")
+#mohm kol 3mlya btt3ml lw7dha e3ny elmafrod tktb 2 we b3d kda enter b3d kda 2 enter w kda we al opration *,-,/,+ fe al akher khales wa shokran 
